948c652abc2a7ce8c5083bdd9a02fe04b9668e9e
Iluvatar <woquendo@gmail.com>
2021-10-27 08:57:44 -0500

    Added example with functions
    Implementation and declaration

---
+++ 2021-10-27-FuncionesI/euler3-functionI.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+bool isprime (long long n); // declaration
+long long largestprimedivisor(long long n);
+
+int main(void)
+{
+    const long long N = 600851475147;
+    std::cout << largestprimedivisor(N) << std::endl;
+
+    return 0;
+}
+
+// implementation
+long long largestprimedivisor(long long n)
+{
+    long long result = n;
+    for (long long ii = 2 ; ii < n ; ii++) {
+        if (n%ii == 0) {
+            long long largedivisor = n/ii;
+            if (isprime(largedivisor) == true) {
+                result = largedivisor;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+
+bool isprime (long long n)
+{
+    bool isprime = true;
+    for(long long jj = 2; jj < n; jj++) {
+        if (n%jj == 0) {
+            isprime = false;
+            break;
+        }
+    }
+    return isprime;
+}
