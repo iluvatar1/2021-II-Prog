9cdb37e9af8c0cae5e7fc102575a748248c4979a
Iluvatar <woquendo@gmail.com>
2021-10-29 08:46:16 -0500

    Added functionality to read command line by using argc and argv in main

---
+++ 2021-10-29-funcionesII/euler3-commandline.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+bool isprime (long long n); // declaration
+long long largestprimedivisor(long long n);
+long long sumprimefactors(long long n);
+
+int main(int argc, char **argv)
+{
+  const long long N = std::atoll(argv[1]);
+  std::cout << argv[1] << std::endl;
+  std::cout << N << std::endl;
+  //const long long N = 600851475147;
+  //const long long N = 105;
+  std::cout << largestprimedivisor(N) << std::endl;
+  std::cout << sumprimefactors(N) << std::endl;
+    
+  return 0;
+}
+
+// implementation
+long long largestprimedivisor(long long n)
+{
+    long long result = n;
+    for (long long ii = 2 ; ii <= n/2 ; ii++) {
+        if (n%ii == 0) {
+            long long largedivisor = n/ii;
+            if (isprime(largedivisor) == true) {
+                result = largedivisor;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+
+bool isprime (long long n)
+{
+    bool isprime = true;
+    for(long long jj = 2; jj <= std::sqrt(n); jj++) {
+        if (n%jj == 0) {
+            isprime = false;
+            break;
+        }
+    }
+    return isprime;
+}
+
+long long sumprimefactors(long long n)
+{
+  long long suma = 0;
+  for (long long ii = 2 ; ii <= n; ii++) {
+    if (n%ii == 0 && isprime(ii)) {
+      suma += ii;
+      n /= ii;
+    }
+  }
+  return suma;
+}
