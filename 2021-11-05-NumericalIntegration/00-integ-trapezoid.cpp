7474c7af4c211bb8d6d20d2933eae67d42d591f4
Iluvatar <woquendo@gmail.com>
2021-11-05 07:53:34 -0500

    Added fixed trapezoid that could also lead to overflows

--- 2021-11-05-NumericalIntegration/00-integ-trapezoid.cpp
+++ 2021-11-05-NumericalIntegration/00-integ-trapezoid.cpp
@@ -18,9 +18,10 @@ int main(int argc, char **argv)
     const double XMAX = std::atof(argv[2]);
     const double H = std::atof(argv[3]);
 
-    for (double h = 1.0e-1; h >= 1.0e-15; h /= 10.0) {
+    const double exact = -std::cos(XMAX) + std::cos(XMIN);
+    for (double h = 1.0e-1; h >= 1.0e-8; h /= 10.0) {
         std::cout << h << "\t"
-                  << std::fabs(1 - trapezoid(XMIN, XMAX, H, fun)/std::cos(X)) << "\n";
+                  << std::fabs(1 - trapezoid(XMIN, XMAX, h, fun)/exact) << "\n";
     }
     return 0;
 }
@@ -35,7 +36,14 @@ double fun(double x)
 template <typename fptr>
 double trapezoid(double a, double b, double h, fptr f)
 {
+  const int n = std::floor((b-a)/h); // warning: (b-a)/h not integer
   double result = 0.0;
+  for(int ii = 1; ii <= n-1; ++ii) {
+    double xi = a + ii*h; // uniform spacing
+    result += f(xi);
+  }
+  result += 0.5*(f(a) + f(b));
+  result *= h;
 
   return result;
 }
