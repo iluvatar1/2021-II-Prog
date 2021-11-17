8d58b3a2ea031f460ff0fc94f65da9c8c88b0cd5
Iluvatar <woquendo@gmail.com>
2021-11-17 08:19:42 -0500

    Added recursive bisection

---
+++ 2021-11-17-RootFinding-II/roots_recursive.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+double f(double x);
+template <class fptr>
+double bisection(double xl, double xu, fptr fun, double eps, int & niter);
+template <class fptr>
+double bisection_recursive(double xl, double xu, fptr fun, double eps, int & niter);
+template <class fptr>
+double regulafalsi(double xl, double xu, fptr fun, double eps, int & niter);
+template <class fptr>
+double newton(double x0, fptr fun, double eps, int & niter);
+
+int main(int argc, char **argv)
+{
+  std::cout.precision(16); std::cout.setf(std::ios::scientific);
+  
+  const double XL = std::atof(argv[1]);
+  const double XU = std::atof(argv[2]);
+  const double EPS = std::atof(argv[3]);
+  int counter = 0;
+  std::cout << bisection(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  counter = 1;
+  std::cout << bisection_recursive(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  std::cout << regulafalsi(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  std::cout << newton(XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+
+  return 0;
+}
+
+double f(double x)
+{
+  return 9.81*(68.1)*(1 - std::exp(-x*10/68.1))/x - 40;
+}
+
+template <class fptr>
+double bisection(double xl, double xu, fptr fun, double eps, int & niter)
+{
+  double xr = xl;
+  int iter = 1;
+  while(std::fabs(fun(xr)) > eps) {
+    if (fun(xr)*fun(xl) < 0) {
+      xu = xr;
+    } else {
+      xl = xr;
+    }
+    xr = (xl + xu)/2; // bisection
+    iter++;
+  }
+  niter = iter;
+  return xr;
+}
+
+template <class fptr>
+double bisection_recursive(double xl, double xu, fptr fun, double eps, int & niter)
+{
+  double xr = (xl + xu)/2; // bisection
+  if (std::fabs(fun(xr)) <= eps) {
+    niter++;
+    return xr;
+  } else {
+    if (fun(xr)*fun(xl) < 0) {
+      xu = xr;
+    } else {
+      xl = xr;
+    }
+    niter++;
+    return bisection_recursive(xl, xu, fun, eps, niter);
+  }
+}
+
+
+template <class fptr>
+double regulafalsi(double xl, double xu, fptr fun, double eps, int & niter)
+{
+  double xr = xl;
+  int iter = 1;
+  while(std::fabs(fun(xr)) > eps) {
+    if (fun(xr)*fun(xl) < 0) {
+      xu = xr;
+    } else {
+      xl = xr;
+    }
+    xr = xu - (f(xu)*(xl-xu))/(f(xl) - f(xu)); // regula falsi
+    iter++;
+  }
+  niter = iter;
+  return xr;
+}
+
+// xi+1 = xi - f(xi)/f'(xi)
+template <class fptr>
+double newton(double x0, fptr fun, double eps, int & niter)
+{
+  double h = 0.00001;
+  double xr = x0;
+  int iter = 1;
+  while(std::fabs(fun(xr)) > eps) {
+    double fderiv =  (fun(xr+h/2) - fun(xr-h/2))/h;
+    xr = xr - fun(xr)/fderiv;
+    iter++;
+  }
+  niter = iter;
+  return xr;
+}
