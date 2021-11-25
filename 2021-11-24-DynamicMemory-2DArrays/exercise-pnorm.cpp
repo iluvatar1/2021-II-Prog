d09532af07922c718bcf05cab6feb6211d8ce138
Iluvatar <woquendo@gmail.com>
2021-11-24 20:56:53 -0500

    Updated exercise

--- 2021-11-24-DynamicMemory-2DArrays/exercise-pnorm.cpp
+++ 2021-11-24-DynamicMemory-2DArrays/exercise-pnorm.cpp
@@ -15,8 +15,11 @@ int main(int argc, char *argv[])
   std::mt19937 gen(0);
   std::uniform_real_distribution<double> dis(1.0, 2.0);
 
-  // declare and fill the array
-  std::vector<double> arr(N);
+  // declare the array
+  std::vector<double> arr;
+  // TODO resize the vector here
+
+  // fill the array using an automatic for
   for (auto & x : arr) {
     x = dis(gen);
   }
@@ -24,11 +27,15 @@ int main(int argc, char *argv[])
   // call the pnorm function
   std::cout << pnorm(arr, P) << std::endl;
 
+  // TODO sort the vector and print the element in the middle
+
+  std::cout << arr[N/2] << std::endl;
+
   return 0;
 }
 
 
 double pnorm(const std::vector<double> & data, int P)
 {
-  // fill here
+  // TODO fill here
 }
