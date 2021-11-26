14f2b6b4bf62a89cde9661930b4d4e12f29df01c
Iluvatar <woquendo@gmail.com>
2021-11-26 09:13:17 -0500

    Added latest examples

--- 2021-11-26-2DArrays/matrix-2D-as-1D.cpp
+++ 2021-11-26-2DArrays/matrix-2D-as-1D.cpp
@@ -5,11 +5,11 @@
      const int W = 4;
      const int H = 3;
 
-   double array2d[H][W] = {{0.0}};
+   double array2d[H*W] = {0.0};
    for (int ii = 0; ii < H; ++ii) {
      for (int jj = 0; jj < W; ++jj) {
-         array2d[ii][jj] = 1.0/(ii+jj+1);
+         array2d[ii*W + jj] = 1.0/(ii+jj+1);
      }
    }
-   std::cout << array2d[1][1] << std::endl;
+   std::cout << array2d[1*W + 1] << std::endl;
  }
